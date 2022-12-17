db39e79af1d6faa080f76c09da48f257a5828ab9
Shirashoji <46389417+Shirashoji@users.noreply.github.com>
2022-12-17 19:34:13 +0900

    fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
                 <p>このページは文理学部情報科学科のWebプログラミングの演習課題です．</p>
                 <p>5421551 白庄司拓真</p>
                 <p>
-                    <a href="https://ip-api.com/">Donate to IP Geolocation API</a>
+                    <a href="https://ip-api.com/">Donate to IP Geolocation API</a><br />
                     <a href="https://mapsplatform.google.com/intl/ja/pricing/">Donate to Google Maps API</a>
                 </p>
             </div>
